f8604499a0799b5eef1807bbe35ea92a99f857fd
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-24 14:47:51 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,8 @@ def my_each (array)
   new_array = []
   i = 0
   while i < array.length
-    new_array.push(yield(array[i]))
+    value = yield(array[i])
+    new_array.push(value)
     i += 1
   end
   new_array

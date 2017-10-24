c69b846b92c1ddd8081c4bf64ee49d6e7d80e934
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-24 14:40:29 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,8 @@ def my_each (array)
   new_array = []
   i = 0
   while i < array.length
-    new_array.push(yield(array[i]))
+    array[i].map do |x| 
+      yield(x)
     i += 1
   end
-  new_array
 end
